d367eb2442a33249ba7d1de1ae0a9e24896e6883
Olaoluwa <olaoluwaoyeyinka@yahoo.com>
2019-03-25 22:04:25 +0100

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 8000;
+
 var app = express();
 
 // hbs declarations
@@ -48,6 +50,6 @@ app.get('/about/developer', (req, res) => {
     });
 });
 
-app.listen(8000, () => {
-    console.log('Server running on port 8000');
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
